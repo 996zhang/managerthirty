8afd5d7ec969f972df202a70092bba06df8a8a4f
Your Name <you@example.com>
2019-05-06 10:46:32 +0800

    '饿了吗ui路由准备完成'

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+//导入饿了么ui
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+
+//导入路由
+import router from "./router"
+
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router,
 }).$mount('#app')
---
+++ src/router.js
@@ -0,0 +1,16 @@
+//导入vue
+import Vue from "vue"
+//导入vuerouter
+import VueRouter from "vue-router"
+//use一下
+Vue.use(VueRouter)
+//导入组件
+
+//路由规则
+const routes = []
+//实例化路由对象
+const router = new VueRouter({
+    routes
+})
+//暴露出去
+export default router
